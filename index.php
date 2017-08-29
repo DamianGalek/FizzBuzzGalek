82cd8ca6f3b003268c277048763dded85dc77dd1
Damian Gałczyński <damian.galczynski@eengine.pl>
2017-08-29 13:19:44 +0200

    Fizz Buz Seven ver 2

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
 <?php 
-
 for($i=1; $i<101; $i++)
 {
-    if($i%3==0) echo "Fizz";
-    if($i%5==0) echo "Buzz";
-    if($i%7==0) echo "Seven";
-    if($i%3!=0 && $i%5!=0 && $i%7!=0) echo $i;
+    $flaga = false;
+    if($i%3==0)
+    { 
+        echo "Fizz";
+        $flaga=true;
+    }
+    if($i%5==0)
+    {
+        echo "Buzz";
+        $flaga=true;
+    }
+    if($i%7==0)
+    {
+        echo "Seven";
+        $flaga=true;
+    } 
+    if($flaga) echo $i;
     echo "</br>";
 }
-
 ?>
