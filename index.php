93614d9e85a055dd3c1cdb05d30d53beb146f327
DamianGalek <damian.galczynski@eengine.pl>
2017-08-29 12:51:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ for($i=1; $i<101; $i++)
 {
     if($i%3==0) echo "Fizz";
     if($i%5==0) echo "Buzz";
-	if($i%7==0) echo "Seven";
+    if($i%7==0) echo "Seven";
     if($i%3!=0 && $i%5!=0 && $i%7!=0) echo $i;
     echo "</br>";
 }
